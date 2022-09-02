e8169e01baf684cec205a274be9873f13d677860
Javier Matusevich <matusevich@hey.com>
2022-09-02 19:38:00 -0300

    feat(app): added static reference data

---
+++ app/src/reference/countries.ts
@@ -0,0 +1,352 @@
+export const countries = [
+  {
+    name: "Andorra",
+    countryCode: "AD",
+    range: ["AD100", "AD700"],
+  },
+  {
+    name: "Argentina",
+    countryCode: "AR",
+    range: ["1601", "9431"],
+  },
+  {
+    name: "American Samoa",
+    countryCode: "AS",
+    range: ["96799", "96799"],
+  },
+  {
+    name: "Austria",
+    countryCode: "AT",
+    range: ["1010", "9992"],
+  },
+  {
+    name: "Australia",
+    countryCode: "AU",
+    range: ["0200", "9726"],
+  },
+  {
+    name: "Bangladesh",
+    countryCode: "BD",
+    range: ["1000", "9461"],
+  },
+  {
+    name: "Belgium",
+    countryCode: "BE",
+    range: ["1000", "9992"],
+  },
+  {
+    name: "Bulgaria",
+    countryCode: "BG",
+    range: ["1000", "9974"],
+  },
+  {
+    name: "Brazil",
+    countryCode: "BR",
+    range: ["01000-000", "99990-000"],
+  },
+  {
+    name: "Canada",
+    countryCode: "CA",
+    range: ["A0A", "Y1A"],
+  },
+  {
+    name: "Switzerland",
+    countryCode: "CH",
+    range: ["1000", "9658"],
+  },
+  {
+    name: "Czech Republic",
+    countryCode: "CZ",
+    range: ["100 00", "798 62"],
+  },
+  {
+    name: "Germany",
+    countryCode: "DE",
+    range: ["01067", "99998"],
+  },
+  {
+    name: "Denmark",
+    countryCode: "DK",
+    range: ["0800", "9990"],
+  },
+  {
+    name: "Dominican Republic",
+    countryCode: "DO",
+    range: ["10101", "11906"],
+  },
+  {
+    name: "Spain",
+    countryCode: "ES",
+    range: ["01001", "52080"],
+  },
+  {
+    name: "Finland",
+    countryCode: "FI",
+    range: ["00002", "99999"],
+  },
+  {
+    name: "Faroe Islands",
+    countryCode: "FO",
+    range: ["100", "970"],
+  },
+  {
+    name: "France",
+    countryCode: "FR",
+    range: ["01000", "98799"],
+  },
+  {
+    name: "Great Britain",
+    countryCode: "GB",
+    range: ["AB1", "ZE3"],
+  },
+  {
+    name: "French Guyana",
+    countryCode: "GF",
+    range: ["97300", "97390"],
+  },
+  {
+    name: "Guernsey",
+    countryCode: "GG",
+    range: ["GY1", "GY9"],
+  },
+  {
+    name: "Greenland",
+    countryCode: "GL",
+    range: ["2412", "3992"],
+  },
+  {
+    name: "Guadeloupe",
+    countryCode: "GP",
+    range: ["97100", "97190"],
+  },
+  {
+    name: "Guatemala",
+    countryCode: "GT",
+    range: ["01001", "22027"],
+  },
+  {
+    name: "Guam",
+    countryCode: "GU",
+    range: ["96910", "96932"],
+  },
+  {
+    name: "Guyana",
+    countryCode: "GY",
+    range: ["97312", "97360"],
+  },
+  {
+    name: "Croatia",
+    countryCode: "HR",
+    range: ["10000", "53296"],
+  },
+  {
+    name: "Hungary",
+    countryCode: "HU",
+    range: ["1011", "9985"],
+  },
+  {
+    name: "Isle of Man",
+    countryCode: "IM",
+    range: ["IM1", "IM9"],
+  },
+  {
+    name: "India",
+    countryCode: "IN",
+    range: ["110001", "855126"],
+  },
+  {
+    name: "Iceland",
+    countryCode: "IS",
+    range: ["101", "902"],
+  },
+  {
+    name: "Italy",
+    countryCode: "IT",
+    range: ["00010", "98168"],
+  },
+  {
+    name: "Jersey",
+    countryCode: "JE",
+    range: ["JE1", "JE3"],
+  },
+  {
+    name: "Japan",
+    countryCode: "JP",
+    range: ["100-0001", "999-8531"],
+  },
+  {
+    name: "Liechtenstein",
+    countryCode: "LI",
+    range: ["9485", "9498"],
+  },
+  {
+    name: "Sri Lanka",
+    countryCode: "LK",
+    range: ["*", "96167"],
+  },
+  {
+    name: "Lithuania",
+    countryCode: "LT",
+    range: ["00001", "99069"],
+  },
+  {
+    name: "Luxembourg",
+    countryCode: "LU",
+    range: ["L-1009", "L-9999"],
+  },
+  {
+    name: "Monaco",
+    countryCode: "MC",
+    range: ["98000", "98000"],
+  },
+  {
+    name: "Moldavia",
+    countryCode: "MD",
+    range: ["MD-2000", "MD-7731"],
+  },
+  {
+    name: "Marshall Islands",
+    countryCode: "MH",
+    range: ["96960", "96970"],
+  },
+  {
+    name: "Macedonia",
+    countryCode: "MK",
+    range: ["1000", "7550"],
+  },
+  {
+    name: "Northern Mariana Islands",
+    countryCode: "MP",
+    range: ["96950", "96952"],
+  },
+  {
+    name: "Martinique",
+    countryCode: "MQ",
+    range: ["97200", "97290"],
+  },
+  {
+    name: "Mexico",
+    countryCode: "MX",
+    range: ["01000", "99998"],
+  },
+  {
+    name: "Malaysia",
+    countryCode: "MY",
+    range: ["01000", "98859"],
+  },
+  {
+    name: "Holland",
+    countryCode: "NL",
+    range: ["1000", "9999"],
+  },
+  {
+    name: "Norway",
+    countryCode: "NO",
+    range: ["0001", "9991"],
+  },
+  {
+    name: "New Zealand",
+    countryCode: "NZ",
+    range: ["0110", "9893"],
+  },
+  {
+    name: "Phillippines",
+    countryCode: "PH",
+    range: ["0400", "9811"],
+  },
+  {
+    name: "Pakistan",
+    countryCode: "PK",
+    range: ["10010", "97320"],
+  },
+  {
+    name: "Poland",
+    countryCode: "PL",
+    range: ["00-001", "99-440"],
+  },
+  {
+    name: "Saint Pierre and Miquelon",
+    countryCode: "PM",
+    range: ["97500", "97500"],
+  },
+  {
+    name: "Puerto Rico",
+    countryCode: "PR",
+    range: ["00601", "00988"],
+  },
+  {
+    name: "Portugal",
+    countryCode: "PT",
+    range: ["1000-001", "9980-999"],
+  },
+  {
+    name: "French Reunion",
+    countryCode: "RE",
+    range: ["97400", "97490"],
+  },
+  {
+    name: "Russia",
+    countryCode: "RU",
+    range: ["101000", "901993"],
+  },
+  {
+    name: "Sweden",
+    countryCode: "SE",
+    range: ["10005", "98499"],
+  },
+  {
+    name: "Slovenia",
+    countryCode: "SI",
+    range: ["1000", "9600"],
+  },
+  {
+    name: "Svalbard & Jan Mayen Islands",
+    countryCode: "SJ",
+    range: ["8099", "9178"],
+  },
+  {
+    name: "Slovak Republic",
+    countryCode: "SK",
+    range: ["010 01", "992 01"],
+  },
+  {
+    name: "San Marino",
+    countryCode: "SM",
+    range: ["47890", "47899"],
+  },
+  {
+    name: "Thailand",
+    countryCode: "TH",
+    range: ["10100", "96220"],
+  },
+  {
+    name: "Turkey",
+    countryCode: "TR",
+    range: ["01000", "81950"],
+  },
+  {
+    name: "United States",
+    countryCode: "US",
+    range: ["00210", "99950"],
+  },
+  {
+    name: "Vatican",
+    countryCode: "VA",
+    range: ["00120", "00120"],
+  },
+  {
+    name: "Virgin Islands",
+    countryCode: "VI",
+    range: ["00801", "00851"],
+  },
+  {
+    name: "Mayotte",
+    countryCode: "YT",
+    range: ["97600", "97680"],
+  },
+  {
+    name: "South Africa",
+    countryCode: "ZA",
+    range: ["0002", "9992"],
+  },
+];

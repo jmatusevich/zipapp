eb0ae802f3eda9aa79c13c1fb32b9b504785e994
Javier Matusevich <matusevich@hey.com>
2022-09-02 14:17:31 -0300

    feat(api): split resolver into a separate file

--- api/src/graphql/ZipCodeQuery.ts
+++ api/src/graphql/ZipCodeQuery.ts
@@ -1,13 +1,12 @@
 import { extendType } from "nexus";
+import zipCodeDetailsResolver from "../resolvers/zipCodeDetailsResolver";
 
 export const ZipCodeQuery = extendType({  // 2
     type: "Query",
     definition(t) {
         t.nullable.field("zipCodeDetails", {   // 3
             type: "ZipCodeDetails",
-            resolve(parent, args, context, info) {    // 4
-                return null;
-            },
+            resolve: zipCodeDetailsResolver
         });
     },
 });
---
+++ api/src/resolvers/zipCodeDetailsResolver.ts
@@ -0,0 +1,7 @@
+import { FieldResolver } from "nexus";
+
+const resolver: FieldResolver<"Query", "zipCodeDetails"> = (parent, args, context, info) => {
+    return null;
+}
+
+export default resolver
